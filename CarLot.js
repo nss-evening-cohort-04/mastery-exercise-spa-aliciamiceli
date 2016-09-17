c23bea19c2409d6f0734d46396d578bebd20988d
Alicia Miceli <aliciamiceli@Alicias-MacBook-Pro.local>
2016-09-16 21:42:49 -0500

    functional app but has bugs

--- CarLot.js
+++ CarLot.js
@@ -1,17 +1,35 @@
 var CarLot = (function(){
 
-var carData = "";
+var inventory = [];
 
 var request = new XMLHttpRequest();
 request.open('GET', 'inventory.json');
 request.send();
-request.addEventListener('load', loadFunction)
+request.addEventListener('load', getInventory)
 
 
-  return carData;
+return getInventory;
 })();
 
-function loadFunction() {
-  carData = JSON.parse(this.responseText);
-  console.log(carData)
+function getInventory() {
+  inventory = JSON.parse(this.responseText).cars;
+  console.log(inventory)
+  parseToDom(inventory);
+}
+
+var parseCarsToDom = "";
+var currentCar;
+
+function parseToDom(inventory) {
+  var counter;
+  for (var i = 0; i < inventory.length; i++) {
+    currentCar = inventory[i];
+    parseCarsToDom += `<div id='currentCar${currentCar.id}' class='col-md-4'><h1>${currentCar.make}</h1>`
+    parseCarsToDom += `<h2>${currentCar.year} ${currentCar.model}</h2>`
+    parseCarsToDom += `<h3>$${currentCar.price}</h3>`
+    parseCarsToDom += `<h4 class='description' id='description_${i}'>${currentCar.description}</h4>`
+    parseCarsToDom += `</div>`
+
+  }
+document.getElementById("container").innerHTML = parseCarsToDom;
 }
--- activateEvents.js
+++ activateEvents.js
@@ -1,3 +1,20 @@
 var activateEvents = (function (){
 
-})(CarLot || {}));
+var input = document.getElementById("input")
+
+input.addEventListener('keyup', inputFunction)
+
+function inputFunction(event) {
+
+    if (input.value == 't') {
+    input.value = document.getElementById('description_0').innerHTML;
+  } else if (input.value == 'p') {
+    input.value = document.getElementById('description_1').innerHTML;
+  } else if (input.value == 'a') {
+    input.value = document.getElementById('description_2').innerHTML;
+  } else if (input.value == 'f') {
+    input.value = document.getElementById('description_3').innerHTML;
+  }
+}
+
+})(CarLot || {});
